326c4dc62908a20b612ba22c1ba2dac46ead3a2f
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2018-02-23 19:06:30 +0100

    Update ficheros.py

--- ficheros.py
+++ ficheros.py
@@ -1,8 +1,8 @@
 from VALID import ns
-
+import subprocess
 def ver_text():
     while True:
-        nombre_fichero=((input("Escriba el nombre del fichero que quiere leer: ")+".txt"))
+        nombre_fichero=((input("Escriba el nombre de un fichero de texto existente: ")+".txt"))
         try:
             fichero=open(nombre_fichero,"r")
             break
@@ -16,7 +16,7 @@ while True:
     print("A)LEER UN TEXTO")
     print("B)CONTAR EL NUMERO DE LINEAS DE TEXTO")
     print("C)CONTAR EL NUMERO DE PALABRAS DEL TEXTO")
-    print("D)CONTAR EL NUMER DE CARACTERES DEL TEXTO")
+    print("D)CONTAR EL NUMER0 DE CARACTERES DEL TEXTO")
     op=input("Introduzca aquí su opción: ")
 
     fichero=ver_text()
@@ -44,6 +44,12 @@ while True:
     elif op==("D"):
         print("El texto consta de",contador,"caracteres")
     print("") 
-        
+    conti=ns(input("¿Desea continuar?: "))
+    if conti==("n"):
+        break
+    else:
+        subprocess.call(["cmd.exe","/C","cls"])
+
+
 
 
