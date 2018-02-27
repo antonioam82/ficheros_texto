c56da2250545e2214d464c876c0b17c19768e83f
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2018-02-27 18:42:31 +0100

    Update ficheros.py

--- ficheros.py
+++ ficheros.py
@@ -17,10 +17,13 @@ while True:
     print("B)CONTAR EL NUMERO DE LINEAS DE TEXTO")
     print("C)CONTAR EL NUMERO DE PALABRAS DEL TEXTO")
     print("D)CONTAR EL NUMER0 DE CARACTERES DEL TEXTO")
+    print("E)BUSCAR UNA PALABRA")
     op=input("Introduzca aquí su opción: ")
-
     fichero=ver_text()
+    if op==("E"):
+        palabra=input("Introduzca palabra a buscar: ")
     contador=0
+    nconta=0
     print("")
     for linea in fichero:
         if op==("A"):
@@ -36,6 +39,11 @@ while True:
                 if i==(" ") or i==('\n'):
                     contador-=1
                 contador+=1
+        elif op==("E"):
+            linea.split(" ")
+            if palabra in linea:
+                contador+=1
+   
     fichero.close()
     if op==("B"):
         print("El texto consta de",contador,"líneas")
@@ -43,11 +51,15 @@ while True:
         print("El texto consta de",contador,"palabras")
     elif op==("D"):
         print("El texto consta de",contador,"caracteres")
+    elif op==("E"):
+        print("La palabrase se encontró",contador,"veces")
     print("") 
     conti=ns(input("¿Desea continuar?: "))
     if conti==("n"):
         break
     else:
         subprocess.call(["cmd.exe","/C","cls"])
+
+
 
 
